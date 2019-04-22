8c55dfca15b6cbc2a1d4dc567fcec61724210bf0
TheOnlySmartBoy <o2jose43@gmail.com>
2019-04-22 15:51:22 +0300

    Updated Ui and finished adding adds

--- app/src/main/java/com/otemainc/masenoecampus/MainActivity.java
+++ app/src/main/java/com/otemainc/masenoecampus/MainActivity.java
@@ -8,11 +8,14 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 private WebView container;
 private ProgressBar progressBar;
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +38,9 @@ private ProgressBar progressBar;
             }
         });
         MobileAds.initialize(this, "ca-app-pub-8916451669489221~4463415772");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
     public void onBackPressed(){
         if(container.canGoBack()){
